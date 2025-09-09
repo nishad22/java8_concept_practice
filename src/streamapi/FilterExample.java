908b15177bf90e01b979f36edcde76afd4c5f7cf
nishad <nishad9689@gmail.com>
2025-09-09 22:21:59 +0530

    commitng files

--- src/streamapi/FilterExample.java
+++ src/streamapi/FilterExample.java
@@ -4,11 +4,13 @@ import model.Student;
 import model.StudentDatabase;
 
 import java.util.List;
+import java.util.Objects;
 
 public class FilterExample {
     public static void main(String[] args) {
 
         List<Student> femaleStudentList = StudentDatabase.getAllStudents().stream()
+                .filter(Objects::nonNull)
                 .filter(student -> student.getGender().equalsIgnoreCase("female"))
                 .toList();
 
